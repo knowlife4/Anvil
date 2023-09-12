0b7b062aebba3c14fd1851a06d870f9f7880601d
knowlife4 <knowlife4@gmail.com>
2023-09-12 04:14:55 -0500

    Drawers and Editors

---
+++ Editor/Interfaces/Drawer.cs
@@ -0,0 +1,26 @@
+﻿using Anvil.Elements;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Anvil.Editor.Interfaces
+{
+    public abstract class Drawer : PropertyDrawer, IInterface
+    {
+        protected SerializedProperty Property { get; private set; }
+
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            Property = property;
+            
+            if(RenderDefault) base.CreatePropertyGUI(property);
+            
+            VisualElement root = Root.GetVisualElement();
+            root.style.flexGrow = 1;
+            return root;
+        }
+
+        public abstract Element Root { get; }
+
+        protected abstract bool RenderDefault { get; }
+    }
+}
---
+++ Runtime/Elements/Functional/Editor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UIElements;
+
+namespace Anvil.Elements.Functional
+{
+    public class Editor : Element<IMGUIContainer>
+    {
+        public Editor(UnityEngine.Object obj)
+        {
+            UnityEditor.Editor editor = UnityEditor.Editor.CreateEditor(obj);
+
+            VisualElement = new(() => { editor.OnInspectorGUI(); });
+        }
+
+        protected override IMGUIContainer VisualElement { get; }
+    }
+}
